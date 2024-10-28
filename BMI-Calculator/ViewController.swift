03bbc6f13ed476fadb4312460bfdd193acafd7f0
Nikita Shyshkin <yatoenough@gmail.com>
2024-10-28 21:35:18 +0100

    Initial Commit

---
+++ BMI-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Calculator
+//
+//  Created by Nikita Shyshkin on 28/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
