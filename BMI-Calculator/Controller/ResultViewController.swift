6e8fc829f3e3a31d748c893c8fd9957a06798432
Nikita Shyshkin <yatoenough@gmail.com>
2024-10-29 22:08:39 +0100

    Created ResultViewController

---
+++ BMI-Calculator/Controller/ResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultViewController.swift
+//  BMI-Calculator
+//
+//  Created by Nikita Shyshkin on 29/10/2024.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
